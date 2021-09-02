17202399957575c69c62a98bf1bb89bbe63eba0b
Olivier.Duperrex <olivier.duperrex@unige.ch>
2021-09-02 20:46:17 +0200

    Add comments

--- 0_run_ME.R
+++ 0_run_ME.R
@@ -1,4 +1,8 @@
-## HERE are the codes to update the Rmd files --------------------
+## 0_run_ME.R --------------------
+## olivier.duperrex@unisante.ch
+## 2021-08-26
+##
+## Run these lines to update the Rmd files
 
 ## check where you are
 here::here()
@@ -9,10 +13,12 @@ here::here()
 # wflow_rename_proj("vaccine_effectiveness_screening_method")
 
 ## workflowr manages the website ------------------------------------
+## when you install it the first time in RStudio and you restart RStudio, it should automatically be uploaded
+## if not, just run load it with pacman
 # pacman::p_load(workflowr)
-wflow_status()
 
-wflow_publish(all = TRUE, 'Update project name')
+# wflow_status()
+
 
 # 01. index.Rmd ----------------------------------------------------
 ## Update when adding a new Rmd page
@@ -28,9 +34,10 @@ wflow_publish('analysis/license.Rmd',
 wflow_publish('README.md', 'Update text')
 
 wflow_publish('analysis/VE_nomogram.Rmd',
-              'Initial commit')
+              'Add SO and refs')
 
 
 
 
 wflow_git_push() ## need to use PAT not password from 2021-08-13
+duper
